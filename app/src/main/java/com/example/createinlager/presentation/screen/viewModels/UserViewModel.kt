c31daca107e749d565bb6b98eaf31f8a3b5bcfdb
Sergey Lyadov <ly4dov.s@yandex.ru>
2025-11-27 15:22:42 +0300

    slide screen complete

--- app/src/main/java/com/example/createinlager/presentation/screen/viewModels/UserViewModel.kt
+++ app/src/main/java/com/example/createinlager/presentation/screen/viewModels/UserViewModel.kt
@@ -57,6 +57,13 @@ class UserViewModel(): ViewModel() {
     private val _resultEmailState = MutableStateFlow<ResultState>(ResultState.Initialized)
     val resultEmailState: StateFlow<ResultState> = _resultEmailState.asStateFlow()
 
+    fun getImage(collectionId:String, iduser: String, image:String): String {
+        val imageUrl =
+            "http://192.168.31.176:8090/api/files/${collectionId}/${iduser}/${image}"
+        Log.i("Image", imageUrl)
+        return imageUrl
+    }
+
     fun Registration(name: String, email: String, password: String) {
         _resultState.value = ResultState.Loading
         viewModelScope.launch {
